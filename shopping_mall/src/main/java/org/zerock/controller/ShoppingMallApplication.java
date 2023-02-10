7ba5fe39fc2557f3e32221276fbea48e11c36ad7
DEVELOPERCOM\quaj6 <quaj628@gmail.com>
2023-02-11 02:27:55 +0900

    commit

---
+++ shopping_mall/src/main/java/org/zerock/controller/ShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallApplication.class, args);
+	}
+
+}
